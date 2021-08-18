aa159eea736a17d181cab1f9cfe016cc5827ca33
Syllas Oliveira <syllas.oneto@sp.senai.br>
2021-08-17 22:33:22 -0300

    Ultima versao

--- js/script_UC09SA1A2.js
+++ js/script_UC09SA1A2.js
@@ -1,86 +1,76 @@
-function validaCampos() {
-    // alert("teste");
-    var todasRegras = true;
-    // alert("Teste4")
-    var name = document.getElementById("name").value;
-    // alert("Teste6")
-    if (!(name.length>=10 && name.length<=80)) {
-        todasRegras = false;
-    }
-    // alert("Teste9")
-    var nickname = document.getElementById("nickname").value;
-    if (!(nickname.length>=2 && nickname.length<=20)) {
-        todasRegras = false;
-    }
+function validaFormulario() {
+    var vName = document.getElementById("name").value;
+    var vNickname = document.getElementById("nickname").value;
+    var vDay = document.getElementById("day").value;
+    var vMonth = document.getElementById("month").value;
+    var vYear = document.getElementById("year").value;
+    var vTeamId = document.getElementById("team_id").value;
+    var vCpf = document.getElementById("cpf");
 
-    var day = document.getElementById("day").value;
-    if (!(day>=1 && day<=31)) {
-        todasRegras = false;
+    var sportList = document.getElementsByName("sport");
+    var sportListSelected = [];
+    if(sportList[0].checked) {
+        sportListSelected.push(sportList[0].id);
     }
-
-    var month = document.getElementById("month").value;
-    if (!(month>=1 && month<=12)) {
-        todasRegras = false;
+    if(sportList[1].checked) {
+        sportListSelected.push(sportList[1].id);
     }
-    // alert("Teste24")
-    var year = document.getElementById("year").value;
-    if (!(year>=1900 && year<=2021)) {
-        todasRegras = false;
+    if(sportList[2].checked) {
+        sportListSelected.push(sportList[2].id);
     }
-
-    var cpf = document.getElementById("cpf");
-    if (!(cpf.value.length==14 && !cpf.validity.patternMismatch)) {
-         todasRegras = false;
+    if(sportList[3].checked) {
+        sportListSelected.push(sportList[3].id);
+    }
+    if(sportList[4].checked) {
+        sportListSelected.push(sportList[4].id);
+    }
+    if(sportList[5].checked) {
+        sportListSelected.push(sportList[5].id);
+    }
+    if(sportList[6].checked) {
+        sportListSelected.push(sportList[6].id);
     }
 
-    var team_id = document.getElementById("team_id").value;
-    if (!(team_id != null)) {
-        todasRegras = false;
-    }
-    
-    var sportsList = document.getElementsByName("sport");
-    var marcado = false
-    if(sportsList[0].checked) {
-        marcado = true;
-    } else 
+    var vResultado = false;
 
-    if(sportsList[1].checked) {
-        marcado = true;
-    } else 
-
-    if(sportsList[2].checked) {
-        marcado = true;
-    } else
-
-    if(sportsList[3].checked) {
-        marcado = true;
-    } else
-
-    if(sportsList[4].checked) {
-        marcado = true;
-    } else
-
-    if(sportsList[5].checked) {
-        marcado = true;
-    } else
-
-    if(sportsList[6].checked) {
-        marcado = true;
-    }
-
-    if (!marcado) {
-        todasRegras = false;
+    if (vName.length>=10 && vName.length<=80) {
+        if (vNickname.length>=2 && vNickname.length<=20) {
+            if(vDay>=1 && vDay<=31) {
+                if(vMonth>=1 && vMonth<=12){
+                    if(vYear>=1900 && vYear<=2021) {
+                        if(vTeamId!="") {
+                            if(vCpf.value.length==14 && vCpf.checkValidity()) {
+                                if(sportListSelected.length>0){
+                                    vResultado = true;
+                                } else {
+                                    vResultado = false;
+                                }
+                            } else {
+                                vResultado = false;
+                            }
+                        } else {
+                            vResultado = false;
+                        }
+                    }
+                } else {
+                    vResultado = false;
+                }
+            } else {
+                vResultado = false;
+            }
+        } else {
+            vResultado = false;
+        }
+    } else {
+        vResultado = false;
     }
 
-    var resultado = document.getElementById("resultado");
-    resultado.innerHTML = todasRegras;
-    // alert("Teste44")
-
-    return todasRegras;
+    var vResultadoP = document.getElementById("pResultado");
+    vResultadoP.innerHTML = vResultado;
 
 }
 
-function retornaJSON() {
+function geraJson() {
 
     var obj_form = {
         name: "",
@@ -91,47 +81,41 @@ function retornaJSON() {
         cpf: "",
         team_id: "",
         sport: []
-      }
+    }
 
-      obj_form.name = document.getElementById("name").value;
-      obj_form.nickname = document.getElementById("nickname").value;
-      obj_form.day = document.getElementById("day").value;
-      obj_form.month = document.getElementById("month").value;
-      obj_form.year = document.getElementById("year").value;
-      obj_form.cpf = document.getElementById("cpf").value;
-      obj_form.team_id = document.getElementById("team_id").value;
+    obj_form.name = document.getElementById("name").value;
+    obj_form.nickname = document.getElementById("nickname").value;
+    obj_form.day = document.getElementById("day").value;
+    obj_form.month = document.getElementById("month").value;
+    obj_form.year = document.getElementById("year").value;
+    obj_form.cpf = document.getElementById("cpf").value;
+    obj_form.team_id = document.getElementById("team_id").value;
 
-      var sportsList = document.getElementsByName("sport");
+    var sportList = document.getElementsByName("sport");
 
-      if(sportsList[0].checked) {
-          obj_form.sport.push(sportsList[0].id)
-      }
+    if(sportList[0].checked) {
+        obj_form.sport.push(sportList[0].id);
+    }
+    if(sportList[1].checked) {
+        obj_form.sport.push(sportList[1].id);
+    }
+    if(sportList[2].checked) {
+        obj_form.sport.push(sportList[2].id);
+    }
+    if(sportList[3].checked) {
+        obj_form.sport.push(sportList[3].id);
+    }
+    if(sportList[4].checked) {
+        obj_form.sport.push(sportList[4].id);
+    }
+    if(sportList[5].checked) {
+        obj_form.sport.push(sportList[5].id);
+    }
+    if(sportList[6].checked) {
+        obj_form.sport.push(sportList[6].id);
+    }
 
-      if(sportsList[1].checked) {
-        obj_form.sport.push(sportsList[1].id)
-      }
-
-      if(sportsList[2].checked) {
-        obj_form.sport.push(sportsList[2].id)
-      }
-
-      if(sportsList[3].checked) {
-        obj_form.sport.push(sportsList[3].id)
-      }
-
-      if(sportsList[4].checked) {
-        obj_form.sport.push(sportsList[4].id)
-      }
-
-      if(sportsList[5].checked) {
-        obj_form.sport.push(sportsList[5].id)
-      }
-
-      if(sportsList[6].checked) {
-        obj_form.sport.push(sportsList[6].id)
-      }
-
-      var json = JSON.stringify(obj_form);
-      var resultado = document.getElementById("resultado");
-      resultado.innerHTML = json;
+    var vJson = JSON.stringify(obj_form);
+    var vResultadoP = document.getElementById("pResultado");
+    vResultadoP.innerHTML = vJson;
 }
